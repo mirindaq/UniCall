7304168bb682671d5650dbdff1746acc928004dd
Viet Hoang <gvhoang30112004@gmail.com>
2026-03-13 13:00:35 +0700

    Add notification and user services with Maven configuration
    - Created notification-service with necessary Maven configuration, including pom.xml, application.yaml, and main application class.
    - Implemented user-service with similar structure and dependencies.
    - Added test classes for both services to ensure context loading.
    - Included Maven wrapper scripts and properties for both services to facilitate builds across different environments.
    - Configured .gitignore and .gitattributes for proper version control management.

---
+++ Backend/friend-service/src/test/java/iuh/fit/friend_service/FriendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.friend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
